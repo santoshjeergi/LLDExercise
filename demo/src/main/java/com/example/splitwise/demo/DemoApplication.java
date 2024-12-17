59e4d0de1893bf723bd6b55bcbbeac0b9cae9b5f
santoshjeergi <santoshjeergi@gmail.com>
2024-12-17 10:31:05 +0530

    new changes

--- demo/src/main/java/com/example/splitwise/demo/DemoApplication.java
+++ demo/src/main/java/com/example/splitwise/demo/DemoApplication.java
@@ -3,9 +3,13 @@ package com.example.splitwise.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableWebMvc
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
